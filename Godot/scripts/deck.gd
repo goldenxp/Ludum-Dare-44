9a7f3ec0b5e31e5984ece01da720bf9a2cd9de70
goldenxp <sunder@goldenxp.com>
2019-04-28 11:18:48 -0700

    Controls, Scoring and Card Animation
    With 3 hours to go, the mess truly begins!

--- Godot/scripts/deck.gd
+++ Godot/scripts/deck.gd
@@ -7,24 +7,94 @@ const MED = 0.5
 const NIL = 0
 const NEG = -1
 
+const EFFECT1 = [ MAX, MED, NIL, NEG ]
+const EFFECT2 = [ NEG, MAX, MED, NIL ]
+const EFFECT3 = [ NIL, NEG, MAX, MED ]
+const EFFECT4 = [ MED, NIL, NEG, MAX ] 
+
+# Photos that are uploaded
 const CARDS = [
-{ "image":preload("res://images/cafelatte.svg"), "effect" : [ MAX, MED, NIL, NEG ] },
-{ "image":preload("res://images/cafelatte.svg"), "effect" : [ MAX, MED, NIL, NEG ] }
-]
-
-const CAPTIONS = [
-{ "text":"KEY_CAPTION1", "effect" : [ MED, MED, NIL, NIL ] },
+{ "image":preload("res://images/cafelatte.svg"), "effect":EFFECT1, "mod-tags":["coffee"] },
+{ "image":preload("res://images/trashybeach.svg"), "effect":EFFECT2, "mod-tags":[] },
+{ "image":preload("res://images/finger.svg"), "effect":EFFECT3, "mod-tags":[] },
+{ "image":preload("res://images/dance.svg"), "effect":EFFECT4, "mod-tags":["expression"] },
 
 ]
+
+# How the Pandering happens 
+const MODS_GENERIC = [
+{ "caption": "Hashtag Blessed", "effect":EFFECT1},
+{ "caption": "Things were better in the past", "effect":EFFECT2 },
+{ "caption": "Am I doing Social Media right, ahyuk?", "effect":EFFECT3 },
+{ "caption": "Shoutout to my peeps", "effect":EFFECT4},
+]
+
+# Pandering for specific content as defined by mod-tags
+const MODS_SPECIFIC = {
+	"coffee": [ { "caption":"Beans from Ethiopia!", "effect":EFFECT1 }  ],
+	"expression": [ { "caption":"I feel most alive with art", "effect":EFFECT4 }  ]
+}
 
 func _ready():
 
 	pass
+
+func PickCard(scores):
+	var index = 0
 	
-func DrawCard():
+	var channelToTarget = 0
+	for i in range(scores.size()):
+		if (scores[i] > scores[channelToTarget]):
+			channelToTarget = i
+		
+	for i in range(CARDS.size()):
+		var effect = CARDS[i]["effect"]
+		var intention = effect[channelToTarget]
+		if (intention == MAX):
+			index = i
+			break
+	
+	return index
+
+func TossCard(nodeCard, offScreenPos):
+	var tween = nodeCard.get_node("Tween")
+	tween.remove_all()
+	tween.interpolate_property(nodeCard,"global_position",
+			nodeCard.get_global_position(),offScreenPos,
+			1,Tween.TRANS_QUAD,Tween.EASE_OUT)
+	tween.interpolate_property(nodeCard,"rotation_degrees",
+			nodeCard.rotation_degrees,
+			rand_range(-1080,1080),
+			1, Tween.TRANS_QUAD, Tween.EASE_IN)
+	tween.start()
+	pass
+
+func DrawCard(index):
 	var inst = unit.instance()
 	
-	# TODO change index to param
-	inst.get_node("Image").set_texture(CARDS[0]["image"])
+	inst.get_node("Image").set_texture(CARDS[index]["image"])
 	
 	return inst
+	
+func GetCaptionsForCard(index, nodeCard):
+	var captions = []
+	
+	for i in range(MODS_GENERIC.size()):
+		captions.append(MODS_GENERIC[i])
+	
+	var tags = CARDS[index]["mod-tags"]
+	for i in range(tags.size()):
+		var extraCaptions = MODS_SPECIFIC[tags[i]]
+		for j in range(extraCaptions.size()):
+			captions.append(extraCaptions[j])
+	
+	UpdateCardCaption(captions[0], nodeCard)
+	
+	return captions
+	
+func UpdateCardCaption(caption, nodeCard):
+	nodeCard.get_node("Caption").set_text(caption["caption"])
+	
+func GetScoreFromCard(index):
+	return CARDS[index]["effect"]
+	
--- Godot/scripts/game.gd
+++ Godot/scripts/game.gd
@@ -1,18 +1,31 @@
 extends Node2D
 
+var windowWidth
+var windowHeight
+var scores = [0, 0, 0, 0]
+var scoreDelta = [0, 0, 0, 0]
+var nodeCard
+var captionIndex = 0
+var captions = []
+
 func _ready():
 	get_tree().connect("screen_resized",self,"Resize")
 	Resize()
+	
+	get_node("HUD/HBoxContainer/Button_Prev").connect("pressed",self,"SelectPrevCaption")
+	get_node("HUD/HBoxContainer/Button_Next").connect("pressed",self,"SelectNextCaption")
+	get_node("HUD/HBoxContainer/Button_Upload").connect("pressed",self,"Upload")
+	
 	pass
 	
 func _process(delta):
 	pass
 
 func Resize():
-	var width = OS.get_window_size().x
-	var height = OS.get_window_size().y
+	windowWidth= OS.get_window_size().x
+	windowHeight = OS.get_window_size().y
 	# Ensure we can see the playing field
-	var zoom = max(800/height, 500/width)
+	var zoom = max(800/windowHeight, 500/windowWidth)
 	get_node("Camera2D").make_current()
 	get_node("Camera2D").set_zoom(Vector2(zoom,zoom))
 	# Adjust the HUD
@@ -20,7 +33,54 @@ func Resize():
 	pass
 
 func Start():
-	var node = Deck.DrawCard()
-	get_node("Table").add_child(node)
+	DrawNewCard()
 	
+	pass
+	
+func SelectPrevCaption():
+	captionIndex -= 1
+	if (captionIndex < 0):
+		captionIndex = captions.size() - 1
+	Deck.UpdateCardCaption(captions[captionIndex], nodeCard)
+	pass
+	
+func SelectNextCaption():
+	captionIndex += 1
+	if (captionIndex >= captions.size()):
+		captionIndex = 0
+	Deck.UpdateCardCaption(captions[captionIndex], nodeCard)
+	pass
+
+func Upload():
+	# Calculate score
+	var scoreFromCaption = captions[captionIndex]["effect"]
+	for i in range(scores.size()):
+		scores[i] += scoreDelta[i]
+		scores[i] += scoreFromCaption[i]
+	
+	var defeat = false
+	# Check for defeat condition
+	
+	var offScreenPos = Vector2(
+		rand_range(-windowWidth, windowWidth), 
+		-windowHeight*2)
+	Deck.TossCard(nodeCard,offScreenPos)
+	
+	DrawNewCard()
+	
+	pass
+	
+func DrawNewCard():
+	
+	# Pick next card based on current score
+	var index = Deck.PickCard(scores)
+	# How this card will affect the score
+	scoreDelta = Deck.GetScoreFromCard(index)
+	# Add card to the table
+	nodeCard = Deck.DrawCard(index)
+	get_node("Table").add_child(nodeCard)	
+	# Get captions for the card and set index to the first
+	captions = Deck.GetCaptionsForCard(index, nodeCard)
+	captionIndex = 0
+
 	pass
